b26c6c6b7803b332b9e784d7de901aa802e50965
Josh <joshuazob@gmail.com>
2026-02-06 13:25:39 -0700

    start fresh

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-// Central route aggregator
-const router = require('express').Router();
-
-router.use('/books', require('./books'));
-// router.use('/users', require('./users'));
-
-module.exports = router;
