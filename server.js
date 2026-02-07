40204d769c0abf743eb656e9c76f78176cbd571d
Josh <joshuazob@gmail.com>
2026-02-07 15:30:17 -0700

    updates

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const routes = require('./routes');
+const errorHandler = require('./middleware/errorHandler');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use('/', routes);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
