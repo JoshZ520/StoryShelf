4857acb47257047de7782497e809d8169a46f027
Josh <joshuazob@gmail.com>
2026-02-07 15:36:07 -0700

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@ const booksRoutes = require('./books');
 
 router.use('/books', booksRoutes);
 
+router.get('/', (req, res) => {
+	res.status(200).json({ message: 'StoryShelf API is running' });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
 
@@ -8,6 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', routes);
 app.use(errorHandler);
 
