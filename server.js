6c29da7768b8c0fda18c0fb5bbe48a771f9a6737
Josh <joshuazob@gmail.com>
2026-02-11 14:53:58 -0700

    updated mongo connection

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
+const connectDb = require('./db/connect');
 
 dotenv.config();
 
@@ -15,6 +16,13 @@ app.use('/', routes);
 app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server listening on ${port}`);
-});
+connectDb()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server listening on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to database:', err.message);
+    process.exit(1);
+  });
